14d830ce0a88d690fc9c93ce5cc550004905a5a3
prolik123 <mattti2015@gmail.com>
2022-10-03 13:31:54 +0200

    created repository

---
+++ project/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example {
+    requires transitive javafx.controls;
+    requires javafx.fxml;
+    requires transitive javafx.media;
+    requires transitive org.json;
+    requires org.apache.commons.collections4;
+    requires json.simple;
+    requires javafx.graphics;
+
+    opens proj to javafx.fxml;
+    exports proj;
+}
---
+++ project/src/main/java/proj/App.java
@@ -0,0 +1,26 @@
+package proj;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        stage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
